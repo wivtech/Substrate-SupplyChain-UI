d142afa69ad40464870c2be832b50b52aa4005f2
damiradrovic1116 <damir@wiv.io>
2020-08-11 01:29:57 +0200

    version fix

--- src/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-import login from "../modules/auth/reducers/reducer"
-import dashboard from "../modules/dashboard/reducers/reducer"
-import send from "../modules/send/reducers/reducer"
-
-export default {
-  login,
-  dashboard,
-  send,
-}
--- src/utils/setApiAuthHeader.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default (accessToken = null) => {
-  if (accessToken) {
-    axios.defaults.headers.common.authorization = `${accessToken}`;
-  } else {
-    delete axios.defaults.headers.common.authorization;
-  }
-};
